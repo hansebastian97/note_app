969ecf85b823b4105c24b744a360dd932b120cfe
hansebastian97 <hansebastian97@gmail.com>
2023-05-22 10:02:06 +0700

    gblok

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const PORT = process.env.PORT || 8080
 
 app.use(cors({
     "origin": "*",
-    "methods": "OPTIONS, GET, POST, PUT",
-    "credentials":'true'
+    "methods": "*"
 }));
 
 //log requests
